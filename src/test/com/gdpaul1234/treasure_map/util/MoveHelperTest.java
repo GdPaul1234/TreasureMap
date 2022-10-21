a2b8a16b924f9ed56a4fd8c037a150dd155de94d
GdPaul1234 <paul.godin1234@outlook.fr>
2022-10-21 11:32:03 +0200

    Refactor MoveHelperTest

--- src/test/com/gdpaul1234/treasure_map/util/MoveHelperTest.java
+++ src/test/com/gdpaul1234/treasure_map/util/MoveHelperTest.java
@@ -6,95 +6,109 @@ import com.gdpaul1234.treasure_map.model.Adventurer;
 import com.gdpaul1234.treasure_map.model.Map;
 import com.gdpaul1234.treasure_map.model.Mountain;
 import com.gdpaul1234.treasure_map.model.Treasure;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoveHelperTest {
-    @Test
-    void givenOrientation_getDx_ShouldReturnNextMove() {
-        assertEquals(-1, MoveHelper.getDx(Orientation.O));
-        assertEquals(1, MoveHelper.getDx(Orientation.E));
-        assertEquals(0, MoveHelper.getDx(Orientation.N));
-        assertEquals(0, MoveHelper.getDx(Orientation.S));
-    }
+    @Nested
+    class describeGetDxAndDy {
+        @Test
+        void givenOrientation_getDx_ShouldReturnNextMove() {
+            assertEquals(-1, MoveHelper.getDx(Orientation.O));
+            assertEquals(1, MoveHelper.getDx(Orientation.E));
+            assertEquals(0, MoveHelper.getDx(Orientation.N));
+            assertEquals(0, MoveHelper.getDx(Orientation.S));
+        }
 
-    @Test
-    void givenOrientation_getDy_ShouldReturnNextMove() {
-        assertEquals(-1, MoveHelper.getDy(Orientation.N));
-        assertEquals(1, MoveHelper.getDy(Orientation.S));
-        assertEquals(0, MoveHelper.getDy(Orientation.O));
-        assertEquals(0, MoveHelper.getDy(Orientation.E));
-    }
-
-    @Test
-    void whenOutofBound_canMove_ShouldReturnFalse() {
-        var map = new Map(2, 2);
-
-        assertFalse(MoveHelper.canMove(map, 2, 1));
-        assertFalse(MoveHelper.canMove(map, 0, 2));
-        assertFalse(MoveHelper.canMove(map, -1, 0));
-        assertFalse(MoveHelper.canMove(map, 0, -1));
-    }
-
-    @Test
-    void whenMountain_canMove_ShouldReturnFalse() {
-        var map = new Map(2, 2);
-        map.addField(new Mountain(1, 0));
-
-        assertFalse(MoveHelper.canMove(map, 1, 0));
-    }
-
-    @Test
-    void whenAdventurer_canMove_ShouldReturnFalse() {
-        var map = new Map(2, 2);
-        map.addField(new Adventurer(1, 0, map,
-                "Paul", "N", "AADADAGGA"));
-
-        assertFalse(MoveHelper.canMove(map, 1, 0));
-    }
-
-    @Test
-    void whenTreasure_canMove_ShouldReturnTrue() {
-        var map = new Map(2, 2);
-        map.addField(new Treasure(1, 0, 5));
-
-        assertTrue(MoveHelper.canMove(map, 1, 0));
-    }
-
-    @Test
-    void whenPlain_canMove_ShouldReturnTrue() {
-        var map = new Map(2, 2);
-
-        assertTrue(MoveHelper.canMove(map, 0, 0));
-    }
-
-    @Test
-    void whenTurnLeft_getNextOrientation() {
-        Orientation[] inputs = {Orientation.N, Orientation.E, Orientation.S, Orientation.O};
-        Orientation[] expecteds = {Orientation.O, Orientation.N, Orientation.E, Orientation.S};
-
-        for (int i = 0; i < inputs.length; i++) {
-            assertEquals(expecteds[i], MoveHelper.getNextOrientation(inputs[i], Movement.G));
+        @Test
+        void givenOrientation_getDy_ShouldReturnNextMove() {
+            assertEquals(-1, MoveHelper.getDy(Orientation.N));
+            assertEquals(1, MoveHelper.getDy(Orientation.S));
+            assertEquals(0, MoveHelper.getDy(Orientation.O));
+            assertEquals(0, MoveHelper.getDy(Orientation.E));
         }
     }
 
-    @Test
-    void whenTurnRight_getNextOrientation() {
-        Orientation[] inputs = {Orientation.N, Orientation.E, Orientation.S, Orientation.O};
-        Orientation[] expecteds = {Orientation.E, Orientation.S, Orientation.O, Orientation.N};
 
-        for (int i = 0; i < inputs.length; i++) {
-            assertEquals(expecteds[i], MoveHelper.getNextOrientation(inputs[i], Movement.D));
+    @Nested
+    class describeCanMove {
+        @Test
+        void whenOutOfBound_canMove_ShouldReturnFalse() {
+            var map = new Map(2, 2);
+
+            assertFalse(MoveHelper.canMove(map, 2, 1));
+            assertFalse(MoveHelper.canMove(map, 0, 2));
+            assertFalse(MoveHelper.canMove(map, -1, 0));
+            assertFalse(MoveHelper.canMove(map, 0, -1));
         }
-    }
 
-    @Test
-    void whenAdvance_getNextOrientation_ShouldNotChangeOrientation() {
-        Orientation[] inputs = {Orientation.N, Orientation.E, Orientation.S, Orientation.O};
+        @Nested
+        class contextObstacle {
+            @Test
+            void whenMountain_canMove_ShouldReturnFalse() {
+                var map = new Map(2, 2);
+                map.addField(new Mountain(1, 0));
 
-        for (Orientation input : inputs) {
-            assertEquals(input, MoveHelper.getNextOrientation(input, Movement.A));
+                assertFalse(MoveHelper.canMove(map, 1, 0));
+            }
+
+            @Test
+            void whenAdventurer_canMove_ShouldReturnFalse() {
+                var map = new Map(2, 2);
+                map.addField(new Adventurer(1, 0, map,
+                        "Paul", "N", "AADADAGGA"));
+
+                assertFalse(MoveHelper.canMove(map, 1, 0));
+            }
+
+            @Test
+            void whenTreasure_canMove_ShouldReturnTrue() {
+                var map = new Map(2, 2);
+                map.addField(new Treasure(1, 0, 5));
+
+                assertTrue(MoveHelper.canMove(map, 1, 0));
+            }
+
+            @Test
+            void whenPlain_canMove_ShouldReturnTrue() {
+                var map = new Map(2, 2);
+
+                assertTrue(MoveHelper.canMove(map, 0, 0));
+            }
+        }
+
+        @Nested
+        class contextMove {
+            @Test
+            void whenTurnLeft_getNextOrientation() {
+                Orientation[] inputs = {Orientation.N, Orientation.E, Orientation.S, Orientation.O};
+                Orientation[] expecteds = {Orientation.O, Orientation.N, Orientation.E, Orientation.S};
+
+                for (int i = 0; i < inputs.length; i++) {
+                    assertEquals(expecteds[i], MoveHelper.getNextOrientation(inputs[i], Movement.G));
+                }
+            }
+
+            @Test
+            void whenTurnRight_getNextOrientation() {
+                Orientation[] inputs = {Orientation.N, Orientation.E, Orientation.S, Orientation.O};
+                Orientation[] expecteds = {Orientation.E, Orientation.S, Orientation.O, Orientation.N};
+
+                for (int i = 0; i < inputs.length; i++) {
+                    assertEquals(expecteds[i], MoveHelper.getNextOrientation(inputs[i], Movement.D));
+                }
+            }
+
+            @Test
+            void whenAdvance_getNextOrientation_ShouldNotChangeOrientation() {
+                Orientation[] inputs = {Orientation.N, Orientation.E, Orientation.S, Orientation.O};
+
+                for (Orientation input : inputs) {
+                    assertEquals(input, MoveHelper.getNextOrientation(input, Movement.A));
+                }
+            }
         }
     }
 }
